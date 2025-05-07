e8e46e7c97186f837ad8584097e6b1488d7a7982
4nupam <101491219+4nupam@users.noreply.github.com>
2025-05-07 10:07:37 +0530

    Update Header.js

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,7 +6,7 @@ function Header() {
     <>
    <div className="header">
             <div className="headerLeft">
-                <Link to="/"><img className="header__icon" src="https://res.cloudinary.com/dikhkks5s/image/upload/v1746592323/pngwing.com_jfj0ju.png" /></Link>
+                <Link to="/"><img className="header__icon" src="https://res.cloudinary.com/dikhkks5s/image/upload/v1746592637/pngwing.com_1_vw2yau.png" /></Link>
                 <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
                 <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
                 <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
